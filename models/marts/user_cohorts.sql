40b37e7364c4ad38369846d8a46e0083fea0f6e6
artinada <artinada@gmail.com>
2026-04-30 17:48:01 +0200

    Create user_cohorts.sql for user activity analysis
    Merged two previous staging tables into one and reformatted date fields. Added calculations for user tenure and filtered out invalid events.

---
+++ models/marts/user_cohorts.sql
@@ -0,0 +1,22 @@
+--The two previous tables (staging) have been merged into one. 
+--Event dates and registration dates have been converted to the year-month format. 
+--Calculated the difference in months (month_offset) between the event and the registration—the user’s tenure at the company. 
+--Added filtering for users with missing registration dates and events with missing dates, excluding test events (where event_type equals test_event)
+
+select
+  u.user_id as uuid,
+  DATE_TRUNC('month', u.signup_ts)::DATE as cohort_month,
+  DATE_TRUNC('month', e.event_ts)::DATE as activity_month,
+  (
+  (extract(year FROM e.event_ts) - extract(year FROM u.signup_ts)) * 12 +
+  (extract(month FROM e.event_ts) - extract(month FROM u.signup_ts))
+  ) as month_offset,
+  u.promo_signup_flag
+from users_clean u
+join events_clean e on u.user_id = e.user_id
+where
+  u.signup_ts is not null
+  and e.event_ts >= u.signup_ts
+  and e.event_ts is not null
+  and e.event_type is not null
+  and e.event_type not in ('test_event', '')
