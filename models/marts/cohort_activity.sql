34e1e7ea4bd34ae378233cff156d73192bdb3b83
artinada <artinada@gmail.com>
2026-04-30 18:28:15 +0200

    Refactor cohort_activity.sql for merged tables and date logic
    Merged staging events and user cohorts into one table. Updated date handling and filtering for events.

--- models/marts/cohort_activity.sql
+++ models/marts/cohort_activity.sql
@@ -1,11 +1,22 @@
+--The two previous tables (staging events and user cohorts) have been merged into one. 
+--Event dates have been converted to the year-month format. 
+--Calculated the difference in months (month_offset) between the event and the registration—the user’s tenure at the company. 
+--Added filtering for events with missing dates, excluding test events (where event_type equals test_event)
 SELECT
     c.user_id,
     c.cohort_month,
     c.channel,
-    a.activity_month,
-    (EXTRACT(YEAR FROM a.activity_month) - EXTRACT(YEAR FROM c.cohort_month)) * 12 +
-    (EXTRACT(MONTH FROM a.activity_month) - EXTRACT(MONTH FROM c.cohort_month)) AS month_number
+    DATE_TRUNC('month', e.event_ts)::DATE as activity_month,
+    (
+      (extract(year FROM e.event_ts) - extract(year FROM c.cohort_month)) * 12 +
+      (extract(month FROM e.event_ts) - extract(month FROM c.cohort_month))
+    ) as month_number,
 FROM {{ ref('user_cohorts') }} c
-JOIN {{ ref('int_user_activity_monthly') }} a
-    ON c.user_id = a.user_id
-WHERE a.activity_month >= c.cohort_month
+JOIN {{ ref('stg_events') }} e
+    ON u.user_id = e.user_id
+WHERE 
+    c.activity_month >= c.cohort_month
+    and e.event_ts >= u.signup_ts
+    and e.event_ts is not null
+    and e.event_type is not null
+    and e.event_type not in ('test_event', '')
